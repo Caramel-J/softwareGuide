9a84e2168270d4245b119252911e3edf2e643243
fyadmin <2170474165@qq.com>
2019-12-23 17:42:12 +0800

    修改配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     themeConfig: {
 		search: true,
-    searchMaxSuggestions: 10,
+        searchMaxSuggestions: 10,
         nav:[ // 导航栏配置
             {text: '风眼官网', link: '/accumulate/' },
             {text: '微博', link: 'https://baidu.com'},
@@ -31,7 +31,13 @@ module.exports = {
 			   }
 		   ]
 	   },
-	   sidebarDepth: 5
+	   sidebarDepth: 5,
+	   repo: 'https://github.com/shangHaiFengYan/softwareGuide/',
+	   editLinks: true,
+	   docsRepo: 'https://github.com/shangHaiFengYan/softwareGuide/',
+	   docsDir: 'docs',
+	   editLinkText: '在GitHub上编辑此页面',
+	   lastUpdated: '上次更新'
     },
 	plugins: [
 	    ['container', {
